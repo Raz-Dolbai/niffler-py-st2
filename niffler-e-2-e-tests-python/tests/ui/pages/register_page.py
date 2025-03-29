f98699fc7da20bc85c5408c928354e796df42b8e
raz_2_three <napersson@mail.ru>
2025-03-29 19:42:33 +0300

    пернес локаторы в инициализатор SpendingPage

--- niffler-e-2-e-tests-python/tests/ui/pages/register_page.py
+++ niffler-e-2-e-tests-python/tests/ui/pages/register_page.py
@@ -1,4 +1,3 @@
-from tests.ui.locators import RegisterLocators
 from selene import browser, have
 
 from tests.ui.pages.base_page import BasePage
@@ -11,39 +10,45 @@ class RegisterPage(BasePage):
         self.expected_password_not_equal_text = "Passwords should be equal"
         self.expected_not_valid_user_data_text = "Allowed username length should be from 3 to 50 characters"
         self.expected_not_valid_password_text = "Allowed password length should be from 3 to 12 characters"
+        self.USERNAME = "input[name=username]"
+        self.PASSWORD = "input[name=password]"
+        self.SUBMIT_PASSWORD = "input[name=passwordSubmit]"
+        self.SIGN_UP_BUTTON = "button.form__submit"
+        self.SUCCESS_TEXT = ".form__paragraph_success"
+        self.ERROR = ".form__error"
 
     @allure.step("Fill username")
     def fill_username(self, username: str):
-        browser.element(RegisterLocators.USERNAME).set_value(username)
+        browser.element(self.USERNAME).set_value(username)
 
     @allure.step("Fill password")
     def fill_password(self, password: str):
-        browser.element(RegisterLocators.PASSWORD).set_value(password)
+        browser.element(self.PASSWORD).set_value(password)
 
     @allure.step("Fill submit password")
     def fill_submit_password(self, password: str):
-        browser.element(RegisterLocators.SUBMIT_PASSWORD).set_value(password)
+        browser.element(self.SUBMIT_PASSWORD).set_value(password)
 
     @allure.step("CLick Sign up")
     def click_sign_up(self):
-        browser.element(RegisterLocators.SIGN_UP_BUTTON).click()
+        browser.element(self.SIGN_UP_BUTTON).click()
 
     @allure.step("Check: success text after register")
     def success_text_after_register(self):
-        assert browser.element(RegisterLocators.SUCCESS_TEXT).should(
+        assert browser.element(self.SUCCESS_TEXT).should(
             have.text(self.expected_success_register_text))
 
     @allure.step("Check: password not equal text")
     def password_not_equal(self):
-        assert browser.element(RegisterLocators.ERROR).should(have.text(self.expected_password_not_equal_text))
+        assert browser.element(self.ERROR).should(have.text(self.expected_password_not_equal_text))
 
     @allure.step("Check: username not valid text")
     def username_not_valid(self):
-        assert browser.element(RegisterLocators.ERROR).should(have.text(self.expected_not_valid_user_data_text))
+        assert browser.element(self.ERROR).should(have.text(self.expected_not_valid_user_data_text))
 
     @allure.step("Check: not valid password text")
     def password_not_valid(self):
-        assert browser.element(RegisterLocators.ERROR).should(have.text(self.expected_not_valid_password_text))
+        assert browser.element(self.ERROR).should(have.text(self.expected_not_valid_password_text))
 
     @allure.step("Register user")
     def register_user(self, username: str, password: str, submit_password: str):
--- niffler-e-2-e-tests-python/tests/ui/pages/spending_page.py
+++ niffler-e-2-e-tests-python/tests/ui/pages/spending_page.py
@@ -1,7 +1,5 @@
 from typing import Literal
-from tests.ui.locators import SpendingLocators, MainLocators
 from selene import browser, have, by, be
-
 from tests.ui.pages.base_page import BasePage
 import allure
 
@@ -11,26 +9,39 @@ class SpendingPage(BasePage):
         self.expected_amount_error_text = "Amount has to be not less then 0.01"
         self.expected_category_empty_text = "Please choose category"
         self.expected_empty_spending_page = "There are no spendings"
+        self.AMOUNT = "input[name=amount]"
+        self.ERROR_MESSAGE = "span[class=input__helper-text]"
+        self.CATEGORY = "input[name=category]"
+        self.DATE = "input[name=date]"
+        self.DESCRIPTION = "input[name=description]"
+        self.ADD = "button[id=save]"
+        self.CANCEL = "button[id=cancel]"
+        self.SPENDING = "[id=spendings]"
+        self.SPENDING_CHECKBOX = ".PrivateSwitchBase-input.css-1m9pwf3"
+        self.DELETE_BUTTON = "[id=delete]"
+        self.CURRENCY = "[id=currency]"
+        self.DELETE_MODAL_BUTTON = "body > div.MuiDialog-root.MuiModal-root.css-126xj0f > div.MuiDialog-container.MuiDialog-scrollPaper.css-ekeie0 > div > div.MuiDialogActions-root.MuiDialogActions-spacing.css-19kha6v > button.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-colorPrimary.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-colorPrimary.css-1v1p78s"
+        self.EDIT_SPEND = ".MuiButtonBase-root.MuiIconButton-root.MuiIconButton-colorPrimary.MuiIconButton-sizeMedium.css-dxoo7k"
 
     @allure.step("Fill amount")
     def fill_amount(self, amount: str):
-        browser.element(SpendingLocators.AMOUNT).set_value(amount)
+        browser.element(self.AMOUNT).set_value(amount)
 
     @allure.step("Fill category")
     def fill_category(self, category: str):
-        browser.element(SpendingLocators.CATEGORY).set_value(category)
+        browser.element(self.CATEGORY).set_value(category)
 
     @allure.step("Fill date")
     def fill_date(self, date: str):
-        browser.element(SpendingLocators.DATE).set_value(date)
+        browser.element(self.DATE).set_value(date)
 
     @allure.step("Fill description")
     def fill_description(self, description: str):
-        browser.element(SpendingLocators.DESCRIPTION).set_value(description)
+        browser.element(self.DESCRIPTION).set_value(description)
 
     @allure.step("Expand list currency")
     def open_currency_value(self):
-        browser.element(SpendingLocators.CURRENCY).click()
+        browser.element(self.CURRENCY).click()
 
     @allure.step("Select USD")
     def choose_currency_usd(self):
@@ -59,44 +70,44 @@ class SpendingPage(BasePage):
 
     @allure.step("Click Add")
     def click_add(self):
-        browser.element(SpendingLocators.ADD).click()
+        browser.element(self.ADD).click()
 
     @allure.step("Click Cancel")
     def click_cancel(self):
-        browser.element(SpendingLocators.CANCEL).set_value()
+        browser.element(self.CANCEL).set_value()
 
     @allure.step("Click Delete")
     def click_delete(self):
-        browser.element(SpendingLocators.DELETE_BUTTON).click()
+        browser.element(self.DELETE_BUTTON).click()
 
     @allure.step("Click Delete modal window")
     def click_delete_modal(self):
-        browser.element(SpendingLocators.DELETE_MODAL_BUTTON).click()
+        browser.element(self.DELETE_MODAL_BUTTON).click()
 
     @allure.step("Choose first spend on table")
     def click_checkbox(self):
-        browser.element(SpendingLocators.SPENDING_CHECKBOX).click()
+        browser.element(self.SPENDING_CHECKBOX).click()
 
     @allure.step("Check: Delete button should be disabled")
     def delete_button_should_be_disabled(self):
-        browser.element(SpendingLocators.DELETE_BUTTON).should(be.disabled)
+        browser.element(self.DELETE_BUTTON).should(be.disabled)
 
     @allure.step("Check: Not valid amount text")
-    def not_valid_amount_value_text(self):
-        assert browser.element(SpendingLocators.ERROR_MESSAGE).should(
+    def should_be_expected_text_after_fill_not_valid_amount(self):
+        assert browser.element(self.ERROR_MESSAGE).should(
             have.text(self.expected_amount_error_text))
 
     @allure.step("Check: expected text")
-    def should_have_expected_text(self, text: str):
-        assert browser.element(SpendingLocators.SPENDING).should(have.text(text))
+    def should_be_expected_text(self, text: str):
+        assert browser.element(self.SPENDING).should(have.text(text))
 
     @allure.step("Check: success delete expected text")
-    def success_delete_text(self):
-        assert browser.element(SpendingLocators.SPENDING).should(have.text(self.expected_empty_spending_page))
+    def should_be_expected_text_after_delete_spend(self):
+        assert browser.element(self.SPENDING).should(have.text(self.expected_empty_spending_page))
 
     @allure.step("Click edit")
     def click_edit_spend(self):
-        browser.element(MainLocators.EDIT_SPEND).click()
+        browser.element(self.EDIT_SPEND).click()
 
     @allure.step("Add spend")
     def add_spending(self, amount: str, category: str, description: str = None, date: str = None):
